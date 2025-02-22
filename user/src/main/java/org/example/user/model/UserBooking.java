c08cdd0483c32ea98d538e8c3f87c1dedbb0162c
YanisAF <yanissf2@gmail.com>
2025-02-22 12:33:27 +0100

    creation of the User class and the UserService

--- user/src/main/java/org/example/user/model/UserBooking.java
+++ user/src/main/java/org/example/user/model/UserBooking.java
@@ -1,4 +1,19 @@
 package org.example.user.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserBooking {
+    private UUID id = UUID.randomUUID();
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private String password;
+
 }
